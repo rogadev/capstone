f4b6e624260c9488968729d8324d1c0850752ac6
vonroga <ryanroga@gmail.com>
2023-01-21 16:47:08 -0800

    Update db.ts
    Forgot to change env import format

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,5 +1,5 @@
 import { createClient } from '@supabase/auth-helpers-sveltekit';
-import { env } from '$env/static/public';
+import { PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY } from '$env/static/public';
 
-const supabaseClient = createClient(env.PUBLIC_SUPABASE_URL, env.PUBLIC_SUPABASE_ANON_KEY);
+const supabaseClient = createClient(PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY);
 export default supabaseClient;
