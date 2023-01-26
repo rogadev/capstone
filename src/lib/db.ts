9f7e80da02e27e14a0a7a76e9abe9f1806dbf238
Roga Dev <ryanroga@gmail.com>
2023-01-26 10:24:07 -0800

    Update db.ts
    Replaced SvelteKit importing of env variables with dotenv, instead.

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,5 +1,6 @@
 import { createClient } from '@supabase/auth-helpers-sveltekit';
-import { PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY } from '$env/static/public';
-
-const supabaseClient = createClient(PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY);
+// import { PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY } from '$env/static/public';
+import * as env from 'dotenv';
+env.config();
+const supabaseClient = createClient(process.env.PUBLIC_SUPABASE_URL, process.env.PUBLIC_SUPABASE_ANON_KEY);
 export default supabaseClient;
