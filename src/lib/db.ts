6535bdfbc8da34055e54ada2009e3278b3715c70
Roga Dev <ryanroga@gmail.com>
2023-01-26 09:39:54 -0800

    Testing

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,5 +1,7 @@
 import { createClient } from '@supabase/auth-helpers-sveltekit';
 import { PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY } from '$env/static/public';
 
+console.log(PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY);
+
 const supabaseClient = createClient(PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY);
 export default supabaseClient;
