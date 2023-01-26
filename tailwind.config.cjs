c8afd60de9d884799f3b7034a0c73a4a442df172
Roga Dev <ryanroga@gmail.com>
2023-01-26 09:03:01 -0800

    Housekeeping

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./src/**/*.{html,js,svelte,ts}'],
+  darkMode: 'class',
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/forms')
+  ]
 };
