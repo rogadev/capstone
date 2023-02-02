39fcb44f093e82d7d70d4a429df2188124fefaf2
Roga Dev <ryanroga@gmail.com>
2023-02-02 15:10:38 -0800

    Update Yaml & Testing

--- tests/test.ts
+++ tests/test.ts
@@ -2,5 +2,5 @@ import { expect, test } from '@playwright/test';
 
 test('Welcome page has a Login button', async ({ page }) => {
 	await page.goto('/');
-	expect(await page.waitForSelector('text=Login')).toBeTruthy();
+	expect(await page.waitForSelector('text=Sign in to your account')).toBeTruthy();
 });
