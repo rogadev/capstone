10c33213bd5728725b17f2e3296dc88afa9c9cf9
Roga Dev <ryanroga@gmail.com>
2023-02-02 18:06:54 -0800

    Update SignInWithFacebook.svelte

--- src/lib/components/auth/SignInWithFacebook.svelte
+++ src/lib/components/auth/SignInWithFacebook.svelte
@@ -3,9 +3,12 @@
 </script>
 
 <div>
+	<!-- TODO Remove disabled and title attributes. -->
 	<button
+		disabled
 		formaction="/?login&provider=facebook"
-		class="inline-flex w-full justify-center rounded-md border border-gray-300 bg-white py-2 px-4 text-sm font-medium text-gray-500 shadow-sm hover:bg-gray-50"
+		class="inline-flex w-full justify-center rounded-md border border-gray-300 bg-white py-2 px-4 text-sm font-medium text-gray-500 shadow-sm hover:bg-gray-50 disabled:opacity-50 disabled:cursor-not-allowed"
+		title="Sign in with Facebook is not available at the moment."
 	>
 		<span class="sr-only">Sign in with Facebook</span>
 		<Icon icon="logos:facebook" class="h-5 w-5" />
