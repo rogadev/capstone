703f3799f5affad78713c01170eec83953bd3506
Roga Dev <ryanroga@gmail.com>
2023-02-02 18:06:57 -0800

    Update SignInWithGithub.svelte

--- src/lib/components/auth/SignInWithGithub.svelte
+++ src/lib/components/auth/SignInWithGithub.svelte
@@ -3,11 +3,14 @@
 </script>
 
 <div>
+	<!-- TODO Remove disabled and title attributes. -->
 	<button
-		formaction="/?login&provider=github"
-		class="inline-flex w-full justify-center rounded-md border border-gray-300 bg-white py-2 px-4 text-sm font-medium text-gray-500 shadow-sm hover:bg-gray-50"
+		disabled
+		formaction="/?login&provider=azure"
+		class="inline-flex w-full justify-center rounded-md border border-gray-300 bg-white py-2 px-4 text-sm font-medium text-gray-500 shadow-sm hover:bg-gray-50 disabled:opacity-50 disabled:cursor-not-allowed"
+		title="Sign in with Microsoft is not available at the moment."
 	>
-		<span class="sr-only">Sign in with GitHub</span>
-		<Icon icon="logos:github-icon" class="h-5 w-5" />
+		<span class="sr-only">Sign in with Microsoft</span>
+		<Icon icon="logos:microsoft-icon" class="h-5 w-5" />
 	</button>
 </div>
