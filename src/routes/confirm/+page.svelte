ab2e4935526beeb89b8ca7c7e1ef12edf310bbc1
Roga Dev <ryanroga@gmail.com>
2023-02-02 18:06:26 -0800

    Adds Confirm Password Page & Functionality

---
+++ src/routes/confirm/+page.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+	import ConfirmationRequired from '$lib/components/auth/ConfirmationRequired.svelte';
+
+	export let email: string;
+</script>
+
+<ConfirmationRequired {email} />
