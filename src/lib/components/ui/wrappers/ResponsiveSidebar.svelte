61546d3d3b5e6ba8d30abb17dff15b9afcb9e065
Roga Dev <ryanroga@gmail.com>
2023-02-10 16:50:34 -0800

    Style Updates WIP

--- src/lib/components/ui/wrappers/ResponsiveSidebar.svelte
+++ src/lib/components/ui/wrappers/ResponsiveSidebar.svelte
@@ -1,10 +1,35 @@
-<div class="flex min-h-[92.7vh] flex-col items-stretch justify-items-stretch md:flex-row">
-	<section
-		class="w-screen bg-slate-200 p-4 dark:bg-dark dark:bg-opacity-60 md:w-[20vw] md:min-w-fit"
-	>
-		<slot name="sidebar" />
+<!-- <script lang="ts">
+	export let label: string;
+	let showMenu = false;
+</script> -->
+
+<div class="grow overflow-auto ">
+	<div class="grid grid-cols-10">
+		<section class="col-span-2 min-w-0">asslakdjflasjdflajksdfdf</section>
+		<main class="col-span-8">sss</main>
+	</div>
+</div>
+
+<!-- <div class="flex flex-col md:flex-row">
+	<section class="col-span-2 min-w-fit bg-slate-200 p-4 dark:bg-dark dark:bg-opacity-60">
+		<div class="flex flex-grow">
+			<div class="grid w-[20vw] min-w-[200px] grid-cols-1">
+				{#if label}
+					<h1>{label}</h1>
+				{/if}
+				<div class="hidden md:block">
+					<slot name="sidebar" />
+				</div>
+				<div class="md:hidden">
+					<button type="button" on:click={() => (showMenu = !showMenu)}>Show/Hide</button>
+					{#if showMenu}
+						<slot name="sidebar" />
+					{/if}
+				</div>
+			</div>
+		</div>
 	</section>
-	<section class="m-4">
+	<section class="col-span-8 m-4 w-full max-w-[80vw]">
 		<slot />
 	</section>
-</div>
+</div> -->
