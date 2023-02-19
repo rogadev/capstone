be512d234910b4066ff71ccb61c35670248e6fb3
Roga Dev <ryanroga@gmail.com>
2023-02-19 09:26:53 -0800

    Initial Prisma Setup

---
+++ src/lib/server/prisma/index.ts
@@ -0,0 +1,8 @@
+import { PrismaClient } from '@prisma/client';
+
+// If an existing Prisma Client instance exists, use that. Otherwise, we'll create one.
+const prisma = global.prisma || new PrismaClient();
+
+if (process.env.NODE_ENV === 'development') global.prisma = prisma;
+
+export { prisma };
