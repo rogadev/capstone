eade97d0d78adf77891ee485585eafb49822da57
Roga Dev <ryanroga@gmail.com>
2023-02-19 09:38:37 -0800

    Adds Lucia Types
    Also initialized the UserAttributes type to our Prisma client User type. This may change later.

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,9 +1,18 @@
 import type { PrismaClient } from "@prisma/client";
 
 declare global {
+	/// <reference types="lucia-auth" />
+	declare namespace Lucia {
+		type Auth = import("$lib/server/lucia").Auth;
+		type UserAttributes = import("@prisma/client").User;
+	}
 	namespace App {
 		// interface Error {}
-		// interface Locals {}
+		interface Locals {
+			validate: import('@lucia-auth/sveltekit').Validate;
+			validateUser: import('@lucia-auth/sveltekit').ValidateUser;
+			setSession: import('@lucia-auth/sveltekit').SetSession;
+		}
 		// interface PageData {}
 		// interface Platform {}
 	}
