2439610ec4be503600e15d37d634216fe691ea8c
Roga Dev <ryanroga@gmail.com>
2023-02-19 10:16:47 -0800

    Adds transformUserData config and correctly injects prismaClient as intended.

--- src/lib/server/auth/index.ts
+++ src/lib/server/auth/index.ts
@@ -1,10 +1,20 @@
 import lucia from "lucia-auth";
 import prisma from "@lucia-auth/adapter-prisma";
 import { dev } from "$app/environment";
+import { prismaClient } from "$lib/server/prisma";
+
+// Transform user data takes in user data and returns what we want the user data to look like when we ask based on the session id or whatever we used to get it. In some cases that's fine, but in other cases we might want to transform it. For example, we might want to return the user's full name instead of their first and last name. We can do that here.
 
 export const auth = lucia({
   adapter: prisma(prismaClient),
-  env: dev ? "DEV" : "PROD"
+  env: dev ? "DEV" : "PROD",
+  transformUserData: (userData) => {
+    return {
+      userId: userData.id,
+      username: userData.username,
+      name: userData.fullName,
+    };
+  }
 });
 
 export type Auth = typeof auth;
