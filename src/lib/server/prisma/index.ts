5a11e2e60bb2d062d58ce4ed62c44755c4aaafa9
Roga Dev <ryanroga@gmail.com>
2023-02-21 17:19:26 -0800

    Fix: Properly exporting Prisma client

--- src/lib/server/prisma/index.ts
+++ src/lib/server/prisma/index.ts
@@ -1,8 +1,8 @@
 import { PrismaClient } from '@prisma/client';
 
 // If an existing Prisma Client instance exists, use that. Otherwise, we'll create one.
-const prisma = global.prisma || new PrismaClient();
+const prismaClient = global.prisma || new PrismaClient();
 
-if (process.env.NODE_ENV === 'development') global.prisma = prisma;
+if (process.env.NODE_ENV === 'development') global.prisma = prismaClient;
 
 export { prismaClient };
