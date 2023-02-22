8a398c293af035a20fc6307f86951f422c478255
Roga Dev <ryanroga@gmail.com>
2023-02-22 14:43:35 -0800

    Updated Register Handling
    Redirected to Login if trying to register as an admin while app is not in development.

--- src/routes/register/+page.server.ts
+++ src/routes/register/+page.server.ts
@@ -1,8 +1,12 @@
 import { auth } from "$lib/server/auth";
 import { fail, redirect } from "@sveltejs/kit";
 import type { Actions, PageServerLoad } from "./$types";
+import { dev } from "$app/environment";
 
 export const load: PageServerLoad = async ({ locals }) => {
+  // If we're in production, redirect to login
+  if (!dev) throw redirect(302, "/login");
+
   // If the user is already logged in, redirect to the dashboard
   const session = await locals.validate();
   if (session) throw redirect(302, "/dashboard");
