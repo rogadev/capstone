4a0e80645283449c31516833d8dc50a3e7fa2783
Roga Dev <ryanroga@gmail.com>
2023-02-21 17:08:44 -0800

    Deleted lib/db/index.ts (Supabase mothball)

--- src/lib/db/index.ts
+++
@@ -1,4 +0,0 @@
-import { PUBLIC_SUPABASE_ANON_KEY, PUBLIC_SUPABASE_URL } from '$env/static/public';
-import { createClient } from '@supabase/auth-helpers-sveltekit';
-const supabaseClient = createClient(PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY);
-export default supabaseClient;
