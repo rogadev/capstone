acfa726a860d83788d29e46b3e3656c855f3e98b
Roga Dev <ryanroga@gmail.com>
2023-02-21 17:13:26 -0800

    WIP - Getting Lucia to handle login requests
    Working on getting social and email/pw sign-in working with Lucia auth.

--- src/routes/login/+page.server.ts
+++ src/routes/login/+page.server.ts
@@ -1,10 +1,28 @@
+import { redirect } from "@sveltejs/kit";
 import type { Action } from "./$types";
+import type { PageServerLoad } from "./$types";
 
-export const actions: Actions = {
+export const load: PageServerLoad = async ({ locals }) => {
+	// If the user is already logged in, redirect to the dashboard
+	const session = await locals.validate();
+	if (session) throw redirect(302, '/dashboard');
+
+};
+
+export const actions: Action = {
 	default: async ({ request }) => {
 		const formData = Object.fromEntries(await request.formData());
 		return {
 			data: formData,
+		};
+	},
+	with: async ({ request }) => {
+		const provider = request.params.provider;
+		console.log(provider);
+		return {
+			data: {
+				provider,
+			},
 		};
 	}
 };
