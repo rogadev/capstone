d2e5e4db0c65a39069be2e8d96c54f31562097eb
Roga <ryanroga@gmail.com>
2023-03-02 13:04:58 -0800

    Update hooks.server.ts
    WIP - attempting to determine if/where/when a user is logged in and how the server is reacting to that user's movement through the app.

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -6,6 +6,12 @@ import { handleHooks } from "@lucia-auth/sveltekit";
 import type { Handle } from "@sveltejs/kit";
 
 export const authHandle: Handle = async ({ resolve, event }) => {
+	// console log if there is a logged in user with the current session
+	if (auth.user) {
+		console.log("User is logged in: ", auth.user);
+	} else {
+		console.log("No user is logged in.");
+	}
 	return resolve(event);
 };
 
