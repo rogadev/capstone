59028e4be4e76e7d8563345b886a940e676d4974
Roga <ryanroga@gmail.com>
2023-03-02 13:13:25 -0800

    Working Register Page.

--- src/routes/register/+page.server.ts
+++ src/routes/register/+page.server.ts
@@ -9,74 +9,101 @@ import { z } from 'zod';
 import { PW_MIN_LENGTH, PW_MAX_LENGTH } from '$lib/server/constants';
 
 const emailRegistrationSchema = z.object({
-  email: z.string().email({ message: "Must enter a valid email." }).min(3, { message: "Must enter an email address to register." }).max(100, { message: "Email address is too long." }),
-  password: z.string().min(PW_MIN_LENGTH, { message: `Password must be at least ${PW_MIN_LENGTH} characters long.` }).max(PW_MAX_LENGTH, { message: `Password must be no more than ${PW_MAX_LENGTH} characters long.` }).regex(/[a-z]/, { message: "Password must contain at least 1 lower case letter." }).regex(/[A-Z]/, { message: "Password must contain at least 1 upper case letter." }).regex(/\d/, { message: "Password must contain at least 1 number." }).regex(/[@$!.%*?&_-]/, { message: "Password must contain at least 1 special character. (@$!%*?&_-)" }),
-  passwordConfirm: z.string().min(1, { message: "Must confirm your password." }).max(PW_MAX_LENGTH + 1),
+  username: z
+    .string({ required_error: "Must enter an email address to register." })
+    .email({ message: "Must enter a valid email." })
+    .min(3, { message: "Must enter an email address to register." })
+    .max(100, { message: "Email address must be no more than 100 characters long." }),
+  password: z
+    .string({ required_error: "Must enter a password to register." })
+    .min(PW_MIN_LENGTH, { message: `Password must be at least ${PW_MIN_LENGTH} characters long.` })
+    .max(PW_MAX_LENGTH, { message: `Password must be no more than ${PW_MAX_LENGTH} characters long.` })
+    .regex(/[a-z]/, { message: "Password must contain at least 1 lower case letter." })
+    .regex(/[A-Z]/, { message: "Password must contain at least 1 upper case letter." })
+    .regex(/\d/, { message: "Password must contain at least 1 number." })
+    .regex(/[@$!.%*?&_-]/, { message: "Password must contain at least 1 special character. (@$!%*?&_-)" }),
+  passwordConfirm: z
+    .string({ required_error: "Must confirm your password." })
+    .min(1, { message: "Must confirm your password." })
+    .max(PW_MAX_LENGTH + 1, { message: "Must confirm your password." })
+}).superRefine(data => {
+  if (data.password !== data.passwordConfirm) {
+    return {
+      path: ['passwordConfirm'],
+      message: "Passwords do not match."
+    };
+  }
+  return null;
 });
+
 type EmailRegistrationData = z.infer<typeof emailRegistrationSchema>;
 
-const handleEmailRegistration = async (formData: EmailRegistrationData, locals) => {
-  console.log('Email registration', formData);
-  const result = emailRegistrationSchema.safeParse(formData);
-  const passwordsMatch = formData.password === formData.passwordConfirm;
-  if (!result.success) {
-    const issues = result.error.issues;
-    const errors = {
-      email: [... new Set(issues.filter((issue) => issue.path[0] === 'email').map((issue) => issue.message))],
-      password: [... new Set(issues.filter((issue) => issue.path[0] === 'password').map((issue) => issue.message))],
-    };
-    errors.passwordConfirm = passwordsMatch ? [] : ['Passwords do not match'];
-    return { errors };
-  }
-  const { email, password } = formData;
-  try {
-    const user = await auth.createUser({
-      key: {
-        providerId: 'email',
-        providerUserId: email,
-        password,
-      },
-      attributes: {
-        email,
-      }
-    });
-    const session = await auth.createSession(user.userId);
-    locals.setSession(session);
-  } catch (error) {
-    if (
-      error instanceof Prisma.PrismaClientKnownRequestError &&
-      error.code === 'P2002' &&
-      error.message?.includes('username')
-    ) {
-      return fail(400, {
-        message: 'Username already in use'
-      });
-    }
-    if (error instanceof LuciaError && error.message === 'AUTH_DUPLICATE_KEY_ID') {
-      return fail(400, {
-        message: 'Username already in use'
-      });
-    }
-    return fail(500, {
-      message: 'Unknown error occurred'
-    });
-  }
+type EmailRegistrationErrors = {
+  username?: string[] | undefined;
+  password?: string[] | undefined;
+  passwordConfirm?: string[] | undefined;
 };
 
-
+const { log } = console;
 export const actions: Actions = {
   default: async ({ request, locals }) => {
-    const formData = Object.fromEntries(
-      await request.formData(),
-    ) as Record<string, string>;
-    await handleEmailRegistration(formData, locals);
+    log('Default action fired.');
+    const formData: EmailRegistrationData = Object
+      .fromEntries(await request.formData());
+    log('Data received: ', formData);
+    const result = emailRegistrationSchema.safeParse(formData);
+    const { username } = formData;
+
+    if (!result.success) {
+      log('Validation failed:');
+      const errors: EmailRegistrationErrors = result.error.flatten().fieldErrors;
+      log(errors);
+      return { username, errors };
+    }
+
+    const { password } = formData;
+
+    try {
+      log('Attempting to admin create user...');
+      const user = await auth.createUser({
+        key: {
+          providerId: 'username',
+          providerUserId: username,
+          password,
+        },
+        attributes: {
+          username,
+        }
+      });
+      log('User created: ', user);
+      const session = await auth.createSession(user.userId);
+      locals.setSession(session);
+    } catch (error) {
+      log('Error occurred creating user: ', error);
+      if (
+        error instanceof Prisma.PrismaClientKnownRequestError &&
+        error.code === 'P2002' &&
+        error.message?.includes('username')
+      ) {
+        return fail(400, {
+          message: 'Username already in use'
+        });
+      }
+      if (error instanceof LuciaError && error.message === 'AUTH_DUPLICATE_KEY_ID') {
+        return fail(400, {
+          message: 'Username already in use'
+        });
+      }
+      return fail(500, {
+        message: 'Unknown error occurred'
+      });
+    }
   }
 };
 
 export const load: PageServerLoad = async ({ locals }) => {
   // If we're in production, redirect to login
   if (!dev) throw redirect(302, "/login");
-
   // If the user is already logged in, redirect to the dashboard
   const session = await locals.validate();
   if (session) throw redirect(302, "/dashboard");
--- src/routes/register/+page.svelte
+++ src/routes/register/+page.svelte
@@ -22,11 +22,12 @@
 		<div class="bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10">
 			<form class="space-y-6 text-black" action="#" method="POST">
 				<div>
-					<label for="email" class="block text-sm font-medium text-gray-700">Email address</label>
+					<label for="username" class="block text-sm font-medium text-gray-700">Email address</label
+					>
 					<div class="mt-1">
 						<input
-							id="email"
-							name="email"
+							id="username"
+							name="username"
 							type="email"
 							required
 							class="block w-full appearance-none rounded-md border border-gray-300 px-3 py-2 placeholder-gray-400 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
