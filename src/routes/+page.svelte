c97036e6b9f9ed6b028a6d6040ee75229b78eec3
Roga <ryanroga@gmail.com>
2023-03-03 13:56:11 -0800

    Homepage & Login UI Update
    Created a clear login path for Drivers and Dispatchers. I may also throw some basic app information on this screen in the future.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,16 +1,33 @@
-<h1>Welcome to the home page.</h1>
+<script>
+	import { onMount } from 'svelte';
+	let driversLink = '/';
+	let dispatchersLink = '/';
+	onMount(async () => {
+		const protocol = window.location.protocol;
+		const domainName = window.location.hostname;
+		driversLink = `${protocol}//app.${domainName}.com/login`;
+		dispatchersLink = `/login`;
+	});
+</script>
 
-<section class="my-8">
-	<p>About the product</p>
-</section>
-
-<section class="my-8">
-	<p>Call to Action</p>
-</section>
-
-<section class="my-8">
-	<div class="flex flex-col items-center justify-center">
-		<a href="/login">Login</a>
-		<a href="/register">Sign Up</a>
+<h1 class="mx-4 mt-8 text-center text-3xl">Log in to get started</h1>
+<div class="mx-8 my-10 grid grid-cols-1 gap-4 md:grid-cols-2">
+	<div class="flex flex-col items-center p-4">
+		<h2 class="mb-4 text-xl font-bold">Drivers</h2>
+		<a
+			href={driversLink}
+			class="rounded-full bg-blue-500 py-2 px-4 font-bold text-white hover:bg-blue-600"
+		>
+			Driving App
+		</a>
 	</div>
-</section>
+	<div class="flex flex-col items-center p-4">
+		<h2 class="mb-4 text-xl font-bold">Dispatchers</h2>
+		<a
+			href={dispatchersLink}
+			class="rounded-full bg-green-500 py-2 px-4 font-bold text-white hover:bg-green-600"
+		>
+			Dispatch Dashboard
+		</a>
+	</div>
+</div>
