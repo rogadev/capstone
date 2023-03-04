603fb172ad3c46145f9115c6b31eeaaa7368504f
Roga <ryanroga@gmail.com>
2023-03-03 18:13:28 -0800

    Updated Route Guarding
    Protected routes in `(protected)` group now auth guarded.

--- src/routes/(admin)/+layout.server.ts
+++ src/routes/(admin)/+layout.server.ts
@@ -1,12 +1,9 @@
+import { redirect } from '@sveltejs/kit';
 import type { LayoutServerLoad } from '../$types';
-// import { redirect } from '@sveltejs/kit';
 
 export const load: LayoutServerLoad = async (event) => {
-  // const session = await getServerSession(event);
-  // if (!session) throw redirect(302, '/login');
-  // const { user } = session;
-  // if (!user) throw redirect(302, '/login');
-  // const isUserAdmin = user?.app_metadata?.roles?.includes('admin');
-  // if (!isUserAdmin) throw redirect(302, '/login');
+  const { locals } = event;
+  const session = await locals.validate();
+  if (!session) throw redirect(301, '/login');
   return {};
 };
