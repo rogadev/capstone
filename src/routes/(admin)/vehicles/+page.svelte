56dff63ffea82927a606e8a7cdc33ffbc0ce9d1e
Roga <ryanroga@gmail.com>
2023-03-09 10:27:17 -0800

    WIP - Admin Driver Pages

---
+++ src/routes/(admin)/vehicles/+page.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import ResponsiveSidebar from '$lib/components/ui/wrappers/ResponsiveSidebar.svelte';
+	const actions = [
+		{
+			label: 'Add Vehicle',
+			icon: 'mdi:plus',
+			href: '/vehicles/add'
+		}
+	];
+</script>
+
+<ResponsiveSidebar title="Vehicles" {actions}>
+ <p>hello</p>
+</ResponsiveSidebar>
---
+++ src/routes/(admin)/vehicles/add/+page.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	import ResponsiveSidebar from '$lib/components/ui/wrappers/ResponsiveSidebar.svelte';
+	const actions = [
+		{
+			label: 'Save',
+			action: () => {
+				console.log('save');
+			}
+		},
+		{
+			label: 'Cancel',
+			action: () => {
+				console.log('cancel');
+			}
+		}
+	];
+</script>
+
+<ResponsiveSidebar title="Add Vehicle" {actions}>
+	<p>hello</p>
+</ResponsiveSidebar>
