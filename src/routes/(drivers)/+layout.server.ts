1f89158ff02e71e452d076678750dc4d39ae05a0
Roga <ryanroga@gmail.com>
2023-03-09 10:27:34 -0800

    WIP - Driver app pages

---
+++ src/routes/(drivers)/+layout.server.ts
@@ -0,0 +1,11 @@
+import { redirect } from '@sveltejs/kit';
+import type { LayoutServerLoad } from '../$types';
+
+export const load: LayoutServerLoad = async (event) => {
+  const { locals } = event;
+  const session = await locals.validate();
+  // TODO also validate if they are a driver
+  // if (!session || !session.user.isDriver) throw redirect(301, '/app/login');
+  if (!session) throw redirect(301, '/app/login');
+  return {};
+};
