e0dd7c2bf46804707fea8698306953a52f9411a8
Roga <ryanroga@gmail.com>
2023-03-09 10:24:27 -0800

    Update hooks.server.ts
    Now checking for authentication the correct way. Only logging if in development.

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -2,12 +2,14 @@ import { auth } from "$lib/server/auth";
 import { sequence } from "@sveltejs/kit/hooks";
 import { handleHooks } from "@lucia-auth/sveltekit";
 import type { Handle } from "@sveltejs/kit";
+import { dev } from "$app/environment"; // TODO Remove for production
 
 export const postAuthHooksHandler: Handle = async ({ resolve, event }) => {
-	if (auth.user) {
-		console.log("User is logged in: ", auth.user);
-	} else {
-		console.log("No user is logged in.");
+	const session = await event.locals.validate();
+	// TODO Remove for production
+	if (dev) {
+		if (!session) console.log('Not authenticated.');
+		else console.log('Authenticated.');
 	}
 	return resolve(event);
 };
