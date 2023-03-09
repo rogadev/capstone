864ba9f7105e0de6c72f0240af2677d1aa8acf3f
Roga <ryanroga@gmail.com>
2023-03-09 12:02:49 -0800

    Homepage Improvements
    - Added user object to the page data via root layout. User object no accessible in all pages via `page` from `$app/stores`.
    - Updated `/` route to display dynamic content based on whether or not user is currently authenticated.
    - Added issue to come back later and improve logged in user options from root route, as well as add redirect to `/app/` if user is not a dispatch driver.

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,4 +1,10 @@
 import { handleServerSession } from '@lucia-auth/sveltekit';
-import type { ServerLoadEvent } from '@sveltejs/kit';
+import type { LayoutServerLoadEvent } from '@sveltejs/kit';
 
-export const load: ServerLoadEvent = handleServerSession();
+export const load: ServerLoadEvent = handleServerSession(async ({ locals }: LayoutServerLoadEvent) => {
+  const { user } = await locals.validateUser();
+  // TODO See issues. Redirect driver users to `/app/` instead of admin homepage.
+  return {
+    user,
+  };
+});
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,7 @@
 <script>
 	import { onMount } from 'svelte';
+	import { page } from '$app/stores';
+
 	let driversLink = '/';
 	let dispatchersLink = '/';
 	onMount(async () => {
@@ -8,26 +10,33 @@
 		driversLink = `${protocol}//app.${domainName}.com/login`;
 		dispatchersLink = `/login`;
 	});
+
+	$: user = $page.data.user;
 </script>
 
-<h1 class="mx-4 mt-8 text-center text-3xl">Log in to get started</h1>
-<div class="mx-8 my-10 grid grid-cols-1 gap-4 md:grid-cols-2">
-	<div class="flex flex-col items-center p-4">
-		<h2 class="mb-4 text-xl font-bold">Drivers</h2>
-		<a
-			href={driversLink}
-			class="rounded-full bg-blue-500 py-2 px-4 font-bold text-white hover:bg-blue-600"
-		>
-			Driving App
-		</a>
+{#if user}
+	<h1>Welcome Back</h1>
+	<!-- TODO Create a better "welcome" screen for admin users. -->
+{:else}
+	<h1 class="mx-4 mt-8 text-center text-3xl">Log in to get started</h1>
+	<div class="mx-8 my-10 grid grid-cols-1 gap-4 md:grid-cols-2">
+		<div class="flex flex-col items-center p-4">
+			<h2 class="mb-4 text-xl font-bold">Drivers</h2>
+			<a
+				href={driversLink}
+				class="rounded-full bg-blue-500 py-2 px-4 font-bold text-white hover:bg-blue-600"
+			>
+				Driving App
+			</a>
+		</div>
+		<div class="flex flex-col items-center p-4">
+			<h2 class="mb-4 text-xl font-bold">Dispatchers</h2>
+			<a
+				href={dispatchersLink}
+				class="rounded-full bg-green-500 py-2 px-4 font-bold text-white hover:bg-green-600"
+			>
+				Dispatch Dashboard
+			</a>
+		</div>
 	</div>
-	<div class="flex flex-col items-center p-4">
-		<h2 class="mb-4 text-xl font-bold">Dispatchers</h2>
-		<a
-			href={dispatchersLink}
-			class="rounded-full bg-green-500 py-2 px-4 font-bold text-white hover:bg-green-600"
-		>
-			Dispatch Dashboard
-		</a>
-	</div>
-</div>
+{/if}
