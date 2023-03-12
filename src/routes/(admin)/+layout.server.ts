b69e7cdf32b5dcac97bbe3638fe5ee8dc0ec19c3
Roga <ryanroga@gmail.com>
2023-03-12 16:14:26 -0700

    Refactored, Moved, or Removed
    Moved and removed several files during a huge refactor of site routes, layouts, and general structure.

--- src/routes/(admin)/+layout.server.ts
+++
@@ -1,9 +0,0 @@
-import { redirect } from '@sveltejs/kit';
-import type { LayoutServerLoad } from '../$types';
-
-export const load: LayoutServerLoad = async (event) => {
-  const { locals } = event;
-  const session = await locals.validate();
-  if (!session) throw redirect(301, '/login');
-  return {};
-};
