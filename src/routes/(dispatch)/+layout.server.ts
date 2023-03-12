5875ce78c7ec1828e1e8d7f2cfdcb77a649cc4a5
Roga <ryanroga@gmail.com>
2023-03-12 16:19:30 -0700

    Create (dispatch) Group +layout.server.ts
    Checks the current user's role. If not an Admin or SuperAdmin, will redirect out of dispatch group and send to app homepage.

---
+++ src/routes/(dispatch)/+layout.server.ts
@@ -0,0 +1,15 @@
+import { redirect } from '@sveltejs/kit';
+import type { LayoutServerLoad } from '../$types';
+import getRole from '$lib/server/utils/getUserRole';
+
+export const load: LayoutServerLoad = async (event) => {
+  const { locals } = event;
+  // AUTHENTICATION
+  const { session, user } = await locals.validateUser();
+  if (!session) throw redirect(301, '/login');
+  // AUTHORIZATION
+  const role = await getRole(user);
+  if (role !== "Admin" || role !== "SuperAdmin") throw redirect(301, "/app");
+
+  return {};
+};
