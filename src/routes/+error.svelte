a3331326fe1d11566e4dd773ba8b6b4e408d13cc
Roga <ryanroga@gmail.com>
2023-03-12 16:29:39 -0700

    Update +error.svelte

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -1,7 +1,33 @@
-<div>
-	<h2>Not found.</h2>
-	<p>
-		Oops. Looks like the page or information you're looking for cannot be found. Check your url and
-		try again.
-	</p>
+<script lang="ts">
+	import '../app.css';
+	import { page } from '$app/stores';
+	import { handleSession } from '@lucia-auth/sveltekit/client';
+	import * as colorTheme from '$lib/stores/colorTheme';
+	import { onDestroy } from 'svelte';
+
+	handleSession(page);
+
+	colorTheme.useBrowserPreference();
+
+	let mode: 'light' | 'dark';
+	$: useDark = mode === 'dark';
+
+	const colorThemeUnsubscribe = colorTheme.mode.subscribe((value) => {
+		const useDark = value === 'dark';
+		mode = useDark ? 'dark' : 'light';
+	});
+	onDestroy(() => {
+		colorThemeUnsubscribe();
+	});
+</script>
+
+<div class={useDark ? 'dark' : ''}>
+	<div class="flex h-screen flex-col items-center justify-center bg-light dark:bg-dark">
+		<h2 class="mb-4 text-3xl font-bold text-gray-800 dark:text-white">
+			{$page.status}: {$page.error?.message}
+		</h2>
+		<a class="rounded bg-primary py-2 px-4 font-bold text-white hover:bg-accent" href="/">
+			Back to Safety
+		</a>
+	</div>
 </div>
