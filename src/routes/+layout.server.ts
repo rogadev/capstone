7f1f4488762992229401174eec27cf6ca9ca4b3c
Roga <ryanroga@gmail.com>
2023-03-12 16:30:03 -0700

    Update +layout.server.ts
    Completed the TODO.

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -3,7 +3,6 @@ import type { LayoutServerLoadEvent } from '@sveltejs/kit';
 
 export const load: ServerLoadEvent = handleServerSession(async ({ locals }: LayoutServerLoadEvent) => {
   const { user } = await locals.validateUser();
-  // TODO See issues. Redirect driver users to `/app/` instead of admin homepage.
   return {
     user,
   };
