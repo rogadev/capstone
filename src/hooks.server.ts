3990da11faa3c8d11a21106d0107bdfac4cab6bc
Roga <ryanroga@gmail.com>
2023-03-13 13:32:29 -0700

    Update hooks.server.ts
    Now attaching a user and session property to locals on every server hit.

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -3,15 +3,20 @@ import { sequence } from "@sveltejs/kit/hooks";
 import { handleHooks } from "@lucia-auth/sveltekit";
 import type { Handle } from "@sveltejs/kit";
 import { dev } from "$app/environment"; // TODO Remove for production
+import getRole from "$lib/server/utils/getUserRole";
 
 export const postAuthHooksHandler: Handle = async ({ resolve, event }) => {
-	const session = await event.locals.validate();
+	const { locals } = event;
+	const { session, user } = await locals.validateUser();
+	locals.session = session;
+	if (session && user) {
+		const role = await getRole(user);
+		user.role = role;
+	}
+	locals.user = user;
 	if (dev) {
-		if (!session) console.log('Not authenticated.');
-		else {
-			console.log('Authenticated.');
-			console.log('User:', session.user);
-		}
+		console.log("User", user);
+		console.log("Session", session);
 	}
 	return resolve(event);
 };
