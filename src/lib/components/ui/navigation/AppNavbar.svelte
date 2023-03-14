181af09d1df5e2348aed4678f1f9963864db37c9
Roga <ryanroga@gmail.com>
2023-03-14 11:16:16 -0700

    New App Navigation
    Adds app navigation component and adds it to a themed layout for '/app' routes.

---
+++ src/lib/components/ui/navigation/AppNavbar.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import NavbarBrand from '$lib/components/ui/navigation/NavbarBrand.svelte';
+	import ThemeToggle from '$lib/components/theme/ThemeToggle.svelte';
+
+	export let mode: Boolean;
+</script>
+
+<div class="min-w-screen m-0 bg-mid py-3 text-dark dark:bg-dark dark:text-white">
+	<div class="mx-auto w-[95%]">
+		<div class="flex flex-row items-center justify-between">
+			<NavbarBrand />
+			<ThemeToggle {mode} />
+		</div>
+	</div>
+</div>
