a6b4c1a01660cc4f49bddf03da89ad46b3370ba3
Roga <ryanroga@gmail.com>
2023-03-16 20:37:52 -0700

    Add Modules

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,13 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-
-})
+  modules: [
+    '@nuxtjs/tailwindcss',
+    '@nuxtjs/eslint-module',
+    '@nuxtjs/color-mode',
+    'nuxt-icon',
+    'trpc-nuxt/module'
+  ],
+  typescript: {
+    strict: true
+  },
+});
