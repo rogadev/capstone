cfe4c03cbeeb34f4e18399acd93978201e246491
Roga <ryanroga@gmail.com>
2023-03-17 16:05:35 -0700

    Update HeroSection.vue

--- components/HeroSection.vue
+++ components/HeroSection.vue
@@ -1,11 +1,11 @@
 <template>
-  <div class="hero-section bg-gradient-to-b from-base-200 to-transparent py-16 px-8 text-white">
-    <div class="container mx-auto">
+  <div class="hero-section bg-gradient-to-b from-base-200 to-transparent py-16 px-8">
+    <div class="container mx-auto max-w-[850px]">
       <div class="text-center">
         <h1 class="text-4xl font-bold mb-4">Welcome to Ryan's Medical Transport Services</h1>
         <p class="text-xl mb-8">We provide reliable and convenient transportation for patients to and from medical
-          appointments. Our dedicated drivers ensure a comfortable and timely experience, allowing you to focus on your
-          health.</p>
+          appointments at a fraction of the cost of a conventional taxi. Our dedicated drivers ensure a comfortable and
+          timely experience, allowing you to focus on your health and appointments.</p>
         <CTAButton href="/request-trip">
           Request a Trip
         </CTAButton>
@@ -19,10 +19,3 @@ export default {
   name: 'HeroSection',
 };
 </script>
-
-<style scoped>
-.btn-primary {
-  background-color: #EF4444;
-  color: white;
-}
-</style>
