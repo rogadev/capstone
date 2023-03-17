3756afb054583dbefb7c61065a4bb337b7f93961
Roga <ryanroga@gmail.com>
2023-03-17 16:05:31 -0700

    Update ContactUsDetails.vue

--- components/ContactUsDetails.vue
+++ components/ContactUsDetails.vue
@@ -1,16 +1,16 @@
 <template>
   <div class="flex justify-evenly">
-    <button class="btn btn-primary gap-2">
-      <Icon name="ic:baseline-local-phone" />
-      <a href="tel:1-800-555-5555" class="text-white">
+    <a href="tel:1-800-555-5555" class="no-underline hover:underline">
+      <button class="btn btn-primary gap-2">
+        <Icon name="ic:baseline-local-phone" />
         <span>1-800-555-5555</span>
-      </a>
-    </button>
-    <button class="btn btn-primary gap-2">
-      <Icon name="ic:baseline-local-phone" />
-      <a href="mailto:email@example.com" class="text-white">
+      </button>
+    </a>
+    <a href="mailto:email@example.com" class="no-underline hover:underline">
+      <button class="btn btn-primary gap-2">
+        <Icon name="ic:baseline-local-phone" />
         <span>email@example.com</span>
-      </a>
-    </button>
+      </button>
+    </a>
   </div>
 </template>
