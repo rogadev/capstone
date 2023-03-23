88da8a003fc1fe51b6427202d9b5b5e217799222
Roga <ryanroga@gmail.com>
2023-03-23 13:23:43 -0700

    Update '/auth/callback'
    Now using reactive auth store

--- pages/auth/callback.vue
+++ pages/auth/callback.vue
@@ -1,10 +1,8 @@
 <script lang="ts" setup>
-// TODO Ask Bill...
-// const supabase = useSupabaseClient();
-// supabase.auth.onAuthStateChange(async (event) => {
-//   if (event === 'SIGNED_IN') {
-//     window.location.href = '/dashboard';
-//   }
-// });
-navigateTo('/dashboard'); // for now...
+import { useAuthStore } from '@/stores/auth';
+const { refreshUser } = useAuthStore();
+refreshUser();
+const { user } = useAuthStore();
+if (user === null) navigateTo('/');
+else navigateTo('/dashboard');
 </script>
