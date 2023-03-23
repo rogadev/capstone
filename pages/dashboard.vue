5b5e1a6ae28f0ef18fc30cd330a60b820216abc0
Roga <ryanroga@gmail.com>
2023-03-23 10:34:52 -0700

    Login Update - Workflow Changed
    - Removed the login page. Now redirects straight to auth provider sign in page.
    - Kept the auth component. See below.
    - Due to an issue with Supabase' Nuxt auth, user is not immediately returned on provider redirect. As such, we can't auth-guard '/dashboard'. Instead, I've kept it public and included user ref-based conditional rendering of the kept Auth component.

--- pages/dashboard.vue
+++ pages/dashboard.vue
@@ -1,9 +1,16 @@
 <script setup lang="ts">
 definePageMeta({
-  middleware: 'auth-guard',
+  layout: 'driver',
+});
+useHead({
+  title: 'Driver Dashboard',
 });
 const user = useSupabaseUser();
 </script>
+
+<!-- Due to an issue with Supabase auth for Nuxt, user is not immediately returned,
+    so we cannot auth guard here or we'll end up in a loop between dashboard and
+    login provider page. -->
 
 <template>
   <DashboardTabs v-if="user" :user="user" />
