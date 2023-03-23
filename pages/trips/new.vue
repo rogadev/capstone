c93a80124ddcb43108579e8bb090390f1ff957d4
Roga <ryanroga@gmail.com>
2023-03-23 10:35:47 -0700

    Changed "new trip" route

--- pages/trips/new.vue
+++ pages/trips/new.vue
@@ -1,3 +1,92 @@
+
+<script lang="ts" setup>
+definePageMeta({
+  layout: 'driver',
+  middleware: 'auth-guard',
+});
+useHead({
+  title: 'New Trip',
+});
+
+const STAGES = [
+  "Trip Arrival Time",
+  "Passenger Details",
+  "Pick Up Location",
+  "Drop Off Location",
+  "Appointment Details",
+  "Accessibility",
+  "Payment",
+  "Other Details",
+  "Review",
+];
+
+const arrivalTime = ref('');
+const arrivalTimeError = ref('');
+
+const dialogOpen = ref(false);
+const currentStage = ref(1);
+
+const handleFormatArrivalTime = () => {
+  if (arrivalTime.value.toLowerCase().includes('am') || arrivalTime.value.toLowerCase().includes('pm')) {
+    const pm = arrivalTime.value.toLowerCase().includes('pm');
+    let [h, m] = arrivalTime.value.split(':');
+    if (!h || !m) return (arrivalTimeError.value = 'Invalid Time. Please enter a valid 24-hour time. (e.g. 14:00)');
+    let hours = parseInt(h);
+    const minutes = parseInt(m);
+    if (pm) hours = hours + 12;
+    if (hours < 0 || hours > 24 || minutes < 0 || minutes > 59) {
+      arrivalTimeError.value = 'Invalid Time';
+    } else {
+      arrivalTimeError.value = '';
+    }
+    const roundedMinutes = Math.round(minutes / 15) * 15;
+    const hh = hours < 10 ? `0${hours}` : hours;
+    const mm = roundedMinutes > 10 ? `${roundedMinutes}` : '00';
+    arrivalTime.value = `${hh}:${mm}`;
+  }
+  if (!arrivalTime.value.includes(':')) {
+    arrivalTime.value = arrivalTime.value.replace(/(\d{2})(\d{2})/, '$1:$2');
+  }
+  let [h, m] = arrivalTime.value.split(':');
+  if (!h || !m) return (arrivalTimeError.value = 'Invalid Time. Please enter a valid 24-hour time. (e.g. 14:00)');
+  const hours = parseInt(h);
+  const minutes = parseInt(m);
+  if (hours < 0 || hours > 23 || minutes < 0 || minutes > 59) {
+    arrivalTimeError.value = 'Invalid Time';
+  } else {
+    arrivalTimeError.value = '';
+  }
+  const roundedMinutes = Math.round(minutes / 15) * 15;
+  const hh = hours < 10 ? `0${hours}` : hours;
+  const mm = roundedMinutes < 10 ? `0${roundedMinutes}` : roundedMinutes;
+  arrivalTime.value = `${hh}:${mm}`;
+};
+
+const handleSubmit = () => {
+  console.log('Handling Form Submit');
+};
+
+const handleCancel = () => {
+  dialogOpen.value = true;
+};
+
+const navigateBack = () => {
+  window.history.back();
+};
+
+const closeDialog = () => {
+  dialogOpen.value = false;
+};
+
+const stepForward = () => {
+  if (currentStage.value < STAGES.length) currentStage.value++;
+  else handleSubmit();
+};
+
+const stepBackward = () => {
+  if (currentStage.value > 1) currentStage.value--;
+};
+</script>
 <template>
   <div id="page-container" class="p-4">
 
@@ -105,84 +194,3 @@
 
   </div>
 </template>
-
-<script lang="ts" setup>
-const STAGES = [
-  "Trip Arrival Time",
-  "Passenger Details",
-  "Pick Up Location",
-  "Drop Off Location",
-  "Appointment Details",
-  "Accessibility",
-  "Payment",
-  "Other Details",
-  "Review",
-];
-
-const arrivalTime = ref('');
-const arrivalTimeError = ref('');
-
-const dialogOpen = ref(false);
-const currentStage = ref(1);
-
-const handleFormatArrivalTime = () => {
-  if (arrivalTime.value.toLowerCase().includes('am') || arrivalTime.value.toLowerCase().includes('pm')) {
-    const pm = arrivalTime.value.toLowerCase().includes('pm');
-    let [h, m] = arrivalTime.value.split(':');
-    if (!h || !m) return (arrivalTimeError.value = 'Invalid Time. Please enter a valid 24-hour time. (e.g. 14:00)');
-    let hours = parseInt(h);
-    const minutes = parseInt(m);
-    if (pm) hours = hours + 12;
-    if (hours < 0 || hours > 24 || minutes < 0 || minutes > 59) {
-      arrivalTimeError.value = 'Invalid Time';
-    } else {
-      arrivalTimeError.value = '';
-    }
-    const roundedMinutes = Math.round(minutes / 15) * 15;
-    const hh = hours < 10 ? `0${hours}` : hours;
-    const mm = roundedMinutes > 10 ? `${roundedMinutes}` : '00';
-    arrivalTime.value = `${hh}:${mm}`;
-  }
-  if (!arrivalTime.value.includes(':')) {
-    arrivalTime.value = arrivalTime.value.replace(/(\d{2})(\d{2})/, '$1:$2');
-  }
-  let [h, m] = arrivalTime.value.split(':');
-  if (!h || !m) return (arrivalTimeError.value = 'Invalid Time. Please enter a valid 24-hour time. (e.g. 14:00)');
-  const hours = parseInt(h);
-  const minutes = parseInt(m);
-  if (hours < 0 || hours > 23 || minutes < 0 || minutes > 59) {
-    arrivalTimeError.value = 'Invalid Time';
-  } else {
-    arrivalTimeError.value = '';
-  }
-  const roundedMinutes = Math.round(minutes / 15) * 15;
-  const hh = hours < 10 ? `0${hours}` : hours;
-  const mm = roundedMinutes < 10 ? `0${roundedMinutes}` : roundedMinutes;
-  arrivalTime.value = `${hh}:${mm}`;
-};
-
-const handleSubmit = () => {
-  console.log('Handling Form Submit');
-};
-
-const handleCancel = () => {
-  dialogOpen.value = true;
-};
-
-const navigateBack = () => {
-  window.history.back();
-};
-
-const closeDialog = () => {
-  dialogOpen.value = false;
-};
-
-const stepForward = () => {
-  if (currentStage.value < STAGES.length) currentStage.value++;
-  else handleSubmit();
-};
-
-const stepBackward = () => {
-  if (currentStage.value > 1) currentStage.value--;
-};
-</script>
