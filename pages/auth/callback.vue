c3932d6183ff5fdffc0d03375f11c93aa42dc713
Roga <ryanroga@gmail.com>
2023-03-23 12:14:49 -0700

    Fixed Auth Routing Issue
    Redirecting from home to '/dashboard' as intended, when user is logged in.

--- pages/auth/callback.vue
+++ pages/auth/callback.vue
@@ -1,13 +1,10 @@
 <script lang="ts" setup>
-onMounted(() => {
-  // TODO Ask Bill...
-  // const supabase = useSupabaseClient();
-  // supabase.auth.onAuthStateChange(async (event) => {
-  //   if (event === 'SIGNED_IN') {
-  //     window.location.href = '/dashboard';
-  //   }
-  // });
-
-  window.location.href = '/dashboard'; // for now...
-});
+// TODO Ask Bill...
+// const supabase = useSupabaseClient();
+// supabase.auth.onAuthStateChange(async (event) => {
+//   if (event === 'SIGNED_IN') {
+//     window.location.href = '/dashboard';
+//   }
+// });
+navigateTo('/dashboard'); // for now...
 </script>
