179cdc479d6bea89c0273daed0f1dcb33e37a705
Roga <ryanroga@gmail.com>
2023-03-25 13:37:59 -0700

    Added ButtonLink component

---
+++ components/Button/Link.vue
@@ -0,0 +1,18 @@
+<template>
+  <NuxtLink :to="to" class="btn" :class="classes">
+    <slot />
+  </NuxtLink>
+</template>
+
+<script lang="ts" setup>
+defineProps({
+  to: {
+    type: String,
+    required: true
+  },
+  classes: {
+    type: String,
+    required: false
+  }
+});
+</script>
