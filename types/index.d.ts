5f8b130c853e61a72f9d42db1ecab5115407b77c
Roga <ryanroga@gmail.com>
2023-03-25 20:14:54 -0700

    Added Global Trip Type

--- types/index.d.ts
+++ types/index.d.ts
@@ -11,4 +11,22 @@ declare global {
     name?: string;
     photo?: string;
   };
+
+  type Trip = {
+    trip_id?: string;
+    trip_date?: string;
+    pickup_time: string;
+    passenger_name: string;
+    passenger_phone: string;
+    pickup_name: string;
+    pickup_location_unit: string;
+    pickup_location_street: string;
+    pickup_location_city: string;
+    drop_off_name: string;
+    drop_off_location_unit: string;
+    drop_off_location_street: string;
+    drop_off_location_city: string;
+    drop_off_time: string;
+    notes: string;
+  };
 }
