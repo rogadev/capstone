d6b3a59a93903b5ce652c01e7b9a3a21617b0c88
Roga <ryanroga@gmail.com>
2023-03-25 23:11:03 -0700

    Fix Supabase Client Export

--- server/db/supabase.ts
+++ server/db/supabase.ts
@@ -1,5 +1,4 @@
-
 import { createClient } from '@supabase/supabase-js';
-
-// Create a single supabase client for interacting with your database
 const supabase = createClient(process.env.PUBLIC_SUPABASE_URL, process.env.PUBLIC_SUPABASE_ANON_KEY);
+
+// export default supabase;
