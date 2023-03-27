cf6030a23cc6c0e31619bce96f4c9bef006919e8
Roga <ryanroga@gmail.com>
2023-03-26 22:26:19 -0700

    Created a Sandbox Route for Component Testing

---
+++ pages/sandbox.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <button @click="() => showModal = true">Show Modal</button>
+    <CancelTripModal v-if="showModal" :cancelCloseModal="cancelCloseModal" :confirmCloseModal="confirmCloseModal" />
+  </div>
+</template>
+
+<script lang="ts" setup>
+const showModal = ref(false);
+
+function cancelCloseModal() {
+  showModal.value = false;
+  console.log('cancelCloseModal');
+}
+function confirmCloseModal() {
+  showModal.value = false;
+  console.log('confirmCloseModal');
+}
+</script>
